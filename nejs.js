7d94653ae63b9c872323da999744222c4bcf5d18
codemob <69110900+commandblox@users.noreply.github.com>
2024-06-07 15:42:39 -0400

    file load structure

--- nejs.js
+++ nejs.js
@@ -3,6 +3,26 @@
 // Made by @codemob-dev and @ReallyBadDeveloper
 
 var file = null;
-async function requestFile() {
-    
+document.getElementById("filetoload").addEventListener("change", ev=>{
+    let fr = new FileReader();
+    fr.onload = function () {
+        if (fr.result.byteLength) {
+            var signature = fr.result.slice(0, 5);
+            if (signature == "NES\x1A") {
+                file = fr.result;
+                load();
+            } else {
+                wrongformat();
+            }
+        }
+    }
+
+    fr.readAsArrayBuffer();
+});
+
+function load() {
+
+}
+function wrongformat() {
+
 }
