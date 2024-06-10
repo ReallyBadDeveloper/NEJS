965add817810b5b142c003a2da8012b7d5bd57f5
codemob <69110900+commandblox@users.noreply.github.com>
2024-06-10 12:10:25 -0400

    skrompt

--- nejs.js
+++ nejs.js
@@ -2,19 +2,24 @@
 // Licensed under GNU General Public License v3
 // Made by @codemob-dev and @ReallyBadDeveloper
 
+// -------------------UTILITY------------------
+const getBitOfNum = (num, bit_index) => !!(num&(1<<bit_index));
+// --------------------------------------------
 
-var asciiDecoder = new TextDecoder("ASCII");
+
+// -------------------LOADING------------------
+let asciiDecoder = new TextDecoder("ASCII");
 document.getElementById("filetoload").addEventListener("change", ev=>{
     let filereader = new FileReader();
     filereader.onload = function (frEvent) {
         if (filereader.result.byteLength) {
-            var signature = asciiDecoder.decode(filereader.result.slice(0, 4));
+            let signature = asciiDecoder.decode(filereader.result.slice(0, 4));
 
             if (signature.toString() == "NES\x1A") {
                 file = ev.target.files[0];
-                load(filereader.result, ev.target.files[0]);
+                onload(filereader.result, ev.target.files[0]);
             } else {
-                wrongformat();
+                onloadwrongformat();
             }
         }
     }
@@ -22,18 +27,51 @@ document.getElementById("filetoload").addEventListener("change", ev=>{
     filereader.readAsArrayBuffer(ev.target.files[0]);
 });
 
-var loadedData = null;
-function load(arraybuffer, file) {
-    var uint8buffer = new Uint8Array(arraybuffer);
-    var dataview = new DataView(arraybuffer);
-    var PRGROM = dataview.getUint8(4);
-    var CHRROM = dataview.getUint8(5);
-    var usesCHRRAM = CHRROM == 0;
-    var flags6 = dataview.getUint8(6);
+const nametableModes = {
+    HORIZONTAL_MIRROR: 0,
+    VERTICAL_MIRROR: 1,
+    FOUR_SCREEN: 2
+}
 
-    console.log(PRGROM, CHRROM, usesCHRRAM, flags6);
+let loadedData = null;
+function onloadsuccess(arraybuffer, file) {
+    let dataview = new DataView(arraybuffer);
+    let PRGROM = dataview.getUint8(4);
+    let CHRROM = dataview.getUint8(5);
+    let usesCHRRAM = CHRROM == 0;
+    let flags6 = dataview.getUint8(6);
+
+    let verticalMirroring = getBitOfNum(flags6, 0);
+    let batteryBackedPRGRAM = getBitOfNum(flags6, 1);
+    let trainer = getBitOfNum(flags6, 2);
+
+    let altNametable = getBitOfNum(flags6, 3);
+    let nametableMode = altNametable ? nametableModes.FOUR_SCREEN : +verticalMirroring;
+
+    let mapperNum = flags6 >> 4;
+
+    let flags7 = dataview.getUint8(7);
+
+    mapperNum |= flags7 & 0b11110000;
+
+    let PRGRAM = dataview.getUint8(8);
+
+    PRGRAM = PRGRAM == 0 ? 1 : PRGRAM;
+
+    loadedData = {
+        PRGROM, // size of PRGROM in 16KB banks
+        CHRROM, // size of CHRROM in 8KB banks
+        usesCHRRAM, // the board uses CHRRAM instead of CHRROM
+        batteryBackedPRGRAM, // the board uses battery backed PRGRAM
+        nametableMode, // the mirror mode of the nametable, one of nametableModes
+        mapperNum, // the mapper number: https://www.nesdev.org/wiki/List_of_mappers
+        PRGRAM // size of PRGRAM in 8KB units
+    }
+
     console.log("Loaded!");
 }
-function wrongformat() {
+
+function onloadwrongformat() {
     console.log("Wrong format!");
 }
+// --------------------------------------------
